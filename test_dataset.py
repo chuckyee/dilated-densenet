65da0d22bf7e59135282b9cb52cbc5dd69510d73
Chuck-Hou Yee <chuckyee@gmail.com>
2017-10-01 14:35:59 -0400

    Image normalize unittest.

--- test_dataset.py
+++ test_dataset.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 
 import unittest
+import numpy as np
 import dataset
 
 class TestDataset(unittest.TestCase):
@@ -31,3 +32,13 @@ class TestDataset(unittest.TestCase):
             self.assertTupleEqual(image.shape, (1, 216, 256))
             self.assertEqual(image.dtype, 'float64')
             self.assertEqual(mask, 'epsilon')
+
+    def test_normalize(self):
+        x = np.array([[1, -1], [-1, 1]], dtype='float32')[None,:,:]
+        y = dataset.normalize(x)
+        np.testing.assert_array_equal(x, y)
+
+        # this array: mean = -0.5, std = 2.5
+        x = np.array([[2, -3], [-3, 2]], dtype='float32')[None,:,:]
+        y = dataset.normalize(x)
+        np.testing.assert_array_equal(y, [[[1, -1], [-1, 1]]])
