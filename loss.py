f5a3701c1245da99b0387acc4963d7d2d1e40ac0
Chuck-Hou Yee <chuckyee@gmail.com>
2017-10-01 12:25:59 -0400

    First pass at implementing training.

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 import torch.nn.functional as F
 
@@ -19,3 +20,18 @@ class CrossEntropyLoss2d(nn.Module):
 
     def forward(self, outputs, targets):
         return self.loss(F.log_softmax(outputs), targets)
+
+
+class SoftDice2d(nn.Module):
+    r"""This criterion is the `soft' dice loss for 2d, also known as the F1
+    score.
+    """
+    def __init__(self, weight=None):
+        super().__init__()
+        self.loss = None        # TODO
+
+    def forward(self, outputs, targets):
+        intersection = outputs * targets # TODO: one-hot encode
+        area1 = outputs.sum(dim=2).sum(dim=2)
+        area2 = targets.sum(dim=2).sum(dim=2)   # Won't work
+        return 2*intersection / (area1 + area2) # TODO: regularize
--- train.py
+++ train.py
@@ -2,28 +2,51 @@
 
 from __future__ import division, print_function
 
+
+import argparse
+import logging
+import numpy as np
+
 import torch
 from torch import nn
+from torch.autograd import Variable
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 
 from dataset import RVSC
 from densenet import DilatedDenseNet
 from loss import CrossEntropyLoss2d
 
+
 def train(args, model):
-    model.train()
+    logging.info("Creating dataloaders...")
 
     input_transform = output_transform = None
     dataset = RVSC(args.datadir, input_transform, output_transform)
+
+    # create train-val split
+    # TODO: setup seed for reproducibility
+    # TODO: setup structure for evaluation on validation set
+    # total_images = len(dataset)
+    # train_images = int((1 - args.val_split) * len(dataset))
+    # train_sampler = SubsetRandomSampler(np.random.choice(total_images, train_images))
+    # loader = DataLoader(dataset, batch_size=args.batch_size, sampler=train_sampler)
     loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
+    logging.info("Construct model...")
+
+    model.train()
+
+    # setup cross entropy loss
     weight = torch.ones(2)
     if args.cuda:
         weight = weight.cuda()
     criterion = CrossEntropyLoss2d(weight)
 
     optimizer = Adam(model.parameters())
+
+    logging.info("Begin training...")
 
     for epoch in range(1, args.num_epochs+1):
         epoch_loss = []
@@ -33,8 +56,8 @@ def train(args, model):
                 images = images.cuda()
                 labels = labels.cuda()
 
-            x = torch.Variable(images)
-            y_true = torch.Variable(labels)
+            x = Variable(images)
+            y_true = Variable(labels)
             y_pred = model(x)
 
             optimizer.zero_grad()
@@ -43,7 +66,10 @@ def train(args, model):
             optimizer.step()
 
             epoch_loss.append(loss.data[0])
-            print(loss.data[0])
+            print(step, loss.data[0])
+
+        save_path = f'tensors-{epoch:03d}.pt'
+        torch.save(model.state_dict(), save_path)
 
 def main(args):
     model = DilatedDenseNet(image_channels=1, num_init_features=12,
@@ -58,17 +84,27 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('--cuda', action='store_true')
-    parser.add_argument('--datadir', type=str)
+    parser.add_argument('--datadir', default='.')
+    parser.add_argument('--log', default='INFO')
+    parser.add_argument('--batch-size', type=int, default=32)
 
     subparsers = parser.add_subparsers(dest='mode')
     subparsers.required = True
 
     parser_train = subparsers.add_parser('train')
     parser_train.add_argument('--num-epochs', type=int, default=100)
-    parser_train.add_argument('--batch-size', type=int, default=1)
+    parser_train.add_argument('--val-split', type=float, default=0.2)
 
     parser_eval = subparsers.add_parser('eval')
 
-    main(parser.parse_args())
+    args = parser.parse_args()
+
+    # setup logging level
+    numeric_level = getattr(logging, args.log.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {args.log}')
+    logging.basicConfig(level=numeric_level)
+
+    main(args)
